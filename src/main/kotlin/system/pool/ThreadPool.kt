5dffa7ef9520a09d5ef00ece050c06f856e91212
SansoNoia <devsanso7110@gmail.com>
2021-03-04 00:51:15 +0900

    rewrite

--- src/main/kotlin/system/pool/ThreadPool.kt
+++ src/main/kotlin/system/pool/ThreadPool.kt
@@ -1,47 +1,50 @@
 package system.pool
 
 import java.lang.Runnable
-import java.time.LocalTime
 
 import kotlinx.coroutines.*
-
 
 import java.util.Queue
 import java.util.LinkedList
 
-
-
-
 import container.Container
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.receiveOrNull
 import worker.Worker
 
 
-class Context(val id : Int,private val container : Container)   {
-    val workerQ : Queue<Worker> = LinkedList<Worker>()
-}
-
-
-private class InternalThread : Runnable {
-    var isRunning = true
-
-    private fun loop(block : () -> Unit) {
-        while(isRunning)block()
-    }
-
-    override fun run() = loop(){
-        runBlocking {
-
+class ThreadPool(val count : Int) {
+    object Stream {
+        private val stream = Channel<Worker>()
+        suspend fun getWorker() = stream.receive()
+        suspend fun send(w : Worker) {
+            stream.send(w)
         }
     }
-
-
-
-    fun off() {isRunning = false}
-}
-
-class ThreadPool(val count : Int) {
     private val threads : List<Thread> = makeThread()
 
+    private class InternalThread : Runnable {
+        var isRunning = true
+
+        private fun loop(block : () -> Unit) {
+            while(isRunning)block()
+        }
+
+        override fun run() = loop(){
+            CoroutineScope(Dispatchers.Default).launch {
+                Stream.getWorker().run()
+            }
+        }
+
+
+
+        fun off() {isRunning = false}
+    }
+    fun sendWorker(w : Worker) {
+        CoroutineScope(Dispatchers.IO).launch {
+            Stream.send(w)
+        }
+    }
     private fun makeThread() = List<Thread>(count) {
         Thread(InternalThread())
     }
