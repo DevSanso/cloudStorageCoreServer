3882261f7bd8c7540adbe0cd92da96a5d7596daa
SansoNoia <devsanso7110@gmail.com>
2021-03-15 00:20:17 +0900

    rename, and add error class

--- src/main/kotlin/errors/errors.kt
+++ src/main/kotlin/errors/errors.kt
@@ -2,8 +2,11 @@ package errors
 import java.lang.Exception
 
 
-class AlreadyExistFileException : Exception()
+open class AlreadyExistException : Exception()
+class AlreadyExistFileException : AlreadyExistException()
 class DbIntegrityViolationException : Exception()
 class CantConvertGrpcArgsException : Exception()
 class NotExistException : Exception()
 class NotMatchingHashException : Exception()
+class ViolationOfAccessException : Exception()
+class OverflowException : ArrayIndexOutOfBoundsException()
